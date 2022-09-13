8f9546ea92e9c9dd02a7eeba2312ba45e348a7e7
Johnson <subede.johnson@gmail.com>
2022-09-13 15:43:58 -0400

    added pug file

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3003;
+
+const server = app.listen(port, () => console.log("Server listening on port " + port));
+
+app.set("view engine", "pug");
+app.set("views", "views")
+
+app.get("/", (req, res, next) => {
+    //Payload - data we are sending to a function or page
+
+    var payload = {
+        pageTitle: "Home"
+    }
+
+
+    res.status(200).render("home");
+})
